6de49657294377cac8d4b4a5f63c1d89bf3a448a
rookiesmile <1473018671@qq.com>
2018-02-02 15:12:23 +0800

    Update spider_image.py

--- spider_image.py
+++ spider_image.py
@@ -4,14 +4,16 @@ import re
 import random
 import os
 from login import LoginHuaBan
+from urllib.parse import unquote
 import time
 
-class Spider_Image(LoginHuaBan):
+
+class SpiderImage(LoginHuaBan):
     """
     爬取图片的的地址
     """
     def __init__(self):
-        super(Spider_Image, self).__init__()
+        super(SpiderImage, self).__init__()
         # self.session = requests.session()
         self.headers['User-Agent'] = '''Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36
          (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'''
@@ -36,7 +38,7 @@ class Spider_Image(LoginHuaBan):
         last_img_pin_id = self.get_home_frist_id()    # 得到第一轮的第一个id
         num = 1
         while True:
-            randoms = "".join([chr(random.randint(65, 90)).lower() for i in range(3)]) + str(random.randint(0, 9))  # 随机生成4个字母和1个数字
+            randoms = "".join([chr(random.randint(65, 90)).lower() for r in range(3)]) + str(random.randint(0, 9))  # 随机生成4个字母和1个数字
             url  = "http://huaban.com/?jd2{}&max={}&limit=20&wfl=1".format(randoms, last_img_pin_id)
             get_json = json.loads(self.session.get(url, cookies=self.cookie).content.decode('utf-8'))
             if get_json['pins'] == []:    #  如果为空列表,则判断爬取完成~
@@ -56,11 +58,27 @@ class Spider_Image(LoginHuaBan):
                 self.download(pin_id, img_key, img_type, floder)    # 下载图片
             last_img_pin_id = get_json['pins'][-1]['pin_id']
 
-    def get_all_image(self):
-        url = "https://huaban.com/all"
-        num = 1
+    def get_all_image(self, args):
+        if args == "all":
+            url = "https://huaban.com/{}".format(args)
+            folder = "all"
+        else:
+            folder = unquote(args)
+            url = "https://huaban.com/search/?q={}&jd5gz4om&page=1&per_page=20&wfl=1".format(args)
+            get_total = self.session.get(url, cookies=self.cookie).content.decode('utf-8')
+            re_search_total = re.compile(r'app.page\["pin_count"\] = (.*?);')
+            get_search_total = re_search_total.search(get_total)
+            search_total = int(get_search_total.groups()[0])
+            if search_total == 0:
+                print("没有搜索到关于 '{}' 的图片".format(args))
+                exit()
+            else:
+                print("搜索到的 '{}' 共计 {} 张".format(folder, search_total))
+        total = 1
+        snum = 2
         while True:
             html = self.session.get(url, cookies=self.cookie).content.decode('utf-8')
+            re_search_total = re.compile(r'app.page["pin_count"] = (.*?);')
             re_pins = re.compile(r'app.page\["pins"\] = \[(.*?)\];')    # 所有数据
             get_pins = re_pins.findall(html)
             re_pin_id = re.compile(r'"pin_id":(.*?),')    # 所有的图片id
@@ -69,7 +87,8 @@ class Spider_Image(LoginHuaBan):
             re_title_description = re.compile(r'"title":"(.*?)", "description":"(.*?)", ')    # 标题和描述
             get_pin_id_key = [i for i in re_pin_id.findall(get_pins[0]) if '}' not in i]    # 用户id和图片的key
             if get_pin_id_key == []:
-                print("爬完啦~共计: {}".format(num))
+                print("爬完啦~共计: {}".format(total))
+                break
             get_title_description = re_title_description.findall(get_pins[0])    # 图片的标题和描述
             get_username = re_username.findall(get_pins[0])
             for i, t, pid, u in zip(re_user_id.findall(get_pins[0]), get_title_description, get_pin_id_key, get_username):
@@ -84,16 +103,19 @@ class Spider_Image(LoginHuaBan):
                 title = get_data_json['title']
                 description = get_data_json['description']
                 types = get_data_json['type'].split('/')[-1]
-                folder = "all"
-                print("正在爬取 发布者: {} 的图片 id: {} Key: {} 标题： {} 描述: {}".format(username, pin_id, key, title, description))
+                print("正在爬取 第 {} 张 发布者: {} 的图片 id: {} Key: {} 标题： {} 描述: {}".format(total, username, pin_id, key, title, description))
                 self.download(pin_id, key, types, folder)
-                num += 1
+                total += 1
             randoms = "".join([chr(random.randint(65, 90)).lower() for i in range(3)]) + str(random.randint(0, 9))  # 随机生成4个字母和1个数字
             last_id = get_data_json['pin_id']
-            url = "https://huaban.com/all/?jd4{}&max={}&limit=20&wfl=1".format(randoms, last_id)
+            if args == "all":
+                url = "https://huaban.com/all/?jd4{}&max={}&limit=20&wfl=1".format(randoms, last_id)
+            else:
+                url = "https://huaban.com/search/?q={}&jd5gz4om&page={}&per_page=20&wfl=1".format(args, snum)
+                snum+=1
             time.sleep(1+random.random())
 
-    def download(self,pin_id, key, img_type, folder):
+    def download(self, pin_id, key, img_type, folder):
         try:
             os.mkdir("{}".format(folder))
         except FileExistsError:
